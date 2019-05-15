d73b64409f6d48478d3953324b7dd9d36357fa4e
YX Hao <lifenjoiner@163.com>
2019-05-16 00:51:24 +0800

    add root dir to title for identification

--- ___Sh___.bat
+++ ___Sh___.bat
@@ -5,4 +5,4 @@ if "%~1"=="" set _M_=K
 Prompt $P ~ $+$_$$$G
 pushd %~dp0
 Call __init__ 2>nul
-start %ComSpec% /%_M_% "Title %USERNAME%'s shell&color 06&set _M_=&%*"
+start %ComSpec% /%_M_% "Title %USERNAME%'s shell %% %~dp0&color 06&set _M_=&%*"
