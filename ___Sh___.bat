5b3445ba6683315068d3eaed198caf5aa41ac6f9
YX Hao <lifenjoiner@163.com>
2020-10-30 19:17:41 +0800

    Less process arguments

--- ___Sh___.bat
+++ ___Sh___.bat
@@ -1,10 +1,12 @@
 @Echo off
 Setlocal
-set _M_=C
-if "%~1"=="" set _M_=K
 Prompt $P ~ $+$_$$$G
 pushd %~dp0
 Call __init__ 2>nul
 path %_path_%%path%
 set _path_=
-start "%USERNAME%'s shell %% %~dp0" %ComSpec% /%_M_% "color 06&set _M_=&%*"
+if "%~1"=="" (
+	start "%USERNAME%'s shell %% %~dp0" %ComSpec% /k "color 06"
+) else (
+	start "%USERNAME%'s shell %% %~dp0" %ComSpec% /c "color 06 &"%*
+)
